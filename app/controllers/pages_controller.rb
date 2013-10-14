4fb7a3e3760ff2533b995d6104b293d64a865077
Roneesh Vashisht <roneesh@gmail.com>
2013-10-14 16:08:07 -0500

    the user can see the homepage, tested only referencing capybara docs

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def calculator
+
+  end
+
+end
---
+++ spec/integration/view_homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'Visiting the homepage' do
+
+  scenario 'User sees the calculator page when coming to the site' do
+    visit root_path
+    expect(page).to have_css "title", text: "Calculator"
+  end
+
+  scenario 'User sees the calculator itself' do
+    visit root_path
+    expect(page).to have_css "div.calculator"
+  end
+
+end
+
